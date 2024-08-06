3bfced48ddbb848cfc25287ddb4a0fbbc6e9ff4c
André Simões <andre.simoesdutra23@gmail.com>
2024-08-06 18:23:50 -0300

    CSharp Base Project

---
+++ CSharp/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => {
+    return Results.Ok("Hello World");
+});
+
+app.Run();
